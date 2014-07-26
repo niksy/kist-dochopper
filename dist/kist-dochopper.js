cea2388a5e1265d0e714c191021729ee2c40a338
Ivan Nikolić <niksy5@gmail.com>
2014-07-26 14:03:15 +0200

    Release 0.2.1

--- dist/kist-dochopper.js
+++ dist/kist-dochopper.js
@@ -1,4 +1,4 @@
-/*! kist-dochopper 0.2.0 - Move elements on page depending on media query. | Author: Ivan Nikolić, 2014 | License: MIT */
+/*! kist-dochopper 0.2.1 - Move elements on page depending on media query. | Author: Ivan Nikolić, 2014 | License: MIT */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var hasOwn = Object.prototype.hasOwnProperty;
 var toString = Object.prototype.toString;
